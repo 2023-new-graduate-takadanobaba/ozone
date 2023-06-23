0f66f6566909159125552721b07562e9d7b6952e
rina.ozone <rina.ozone@host.docker.internal>
2023-06-23 11:24:23 +0900

    6/22の成果物

--- src/test/Number.java
+++ src/test/Number.java
@@ -1,10 +1,31 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Number {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	public static void main(String[] args) throws IOException {
+		// 入力
+		
+		System.out.println("数字を入力してください。");
+		
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String str = reader.readLine();
+		
+		int num;
+		// 処理　
+		try {
+			num = Integer.parseInt(str); //数値に型変換
+			 System.out.println(num + "これは数字です。");
+		} catch (Exception unNumber) {
+			 System.out.println("数字ではありません。");
+		} finally {
+			 System.out.println("終了しました。");
+		}
+			
 	}
 
 }
