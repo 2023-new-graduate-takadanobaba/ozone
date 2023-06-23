9c624df674f35dc8feeed7f03eeb946da8c07c64
rina.ozone <rina.ozone@host.docker.internal>
2023-06-23 15:33:38 +0900

    王さんにアドバイスもらって実行できた✨✨✨✨

--- src/test/MultipleOf3.java
+++ src/test/MultipleOf3.java
@@ -7,6 +7,7 @@ public class MultipleOf3 {
 
 	public static void main(String[] args) throws Exception {
 		int num =0;
+		boolean notNum = false;
 		
 		// 出力
 		System.out.println("数字を入力してください。");
@@ -17,28 +18,18 @@ public class MultipleOf3 {
 //		★ここから繰り返しスタート
 		do {	
 			String str = reader.readLine();
+			notNum=false;
 			
 	//		処理 文字列を数値に変換する
 			try {
 				num = Integer.parseInt(str);
 				
-//				if (num%3 == 0) {
-//					System.out.println("3の倍数です。");
-//					System.out.println("以上でシステムを終了します。");
-//				}
-//				else  {
-//					System.out.println("3の倍数ではありません。");
-//					System.out.println("以上でシステムを終了します。");
-//				}
-				
-			} catch (NumberFormatException notNum) {
+			} catch (NumberFormatException Num) {
 				System.out.println("数字を入力してください。2");
-	//			★に戻る
-				
+				notNum=true;
 			} 
 			
-		} while(num%3 == 0 || num%3 != 0);
-			System.out.println(num);
+		} while(notNum==true);
 		
 		if (num%3 == 0) {
 			System.out.println("3の倍数です。");
