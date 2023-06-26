351857990ed8543d8adda137bb8adec985d030b7
rina.ozone <rina.ozone@host.docker.internal>
2023-06-26 10:57:51 +0900

    JUnit5作った

---
+++ src/test/TDDtraining.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TDDtraining {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
